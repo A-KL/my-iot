780592ce22435d0898a1787e562ab9c407cd5dfb
A-KL <myinfo256@yandex.ru>
2015-12-08 23:11:57 +0100

    Initial commit
    Web camera MJpeg streamer

---
+++ projects/VideoCameraStreamer/VideoCameraStreamer/MainPage.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System.IO;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace VideoCameraStreamer
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Threading.Tasks;
+    using Windows.Graphics.Imaging;
+    using Windows.Media;
+    using Windows.Media.Capture;
+    using Windows.Media.MediaProperties;
+    using Windows.UI.Xaml.Controls;
+
+    //[ComImport]
+    //[Guid("5b0d3235-4dba-4d44-865e-8f1d0e4fd04d")]
+    //[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    //unsafe interface IMemoryBufferByteAccess
+    //{
+    //    void GetBuffer(out byte* buffer, out uint capacity);
+    //}
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage"/> class.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Init();
+        }
+
+        /// <summary>
+        /// The init.
+        /// </summary>
+        private async void Init()
+        {
+            var mediaCapture = new MediaCapture();
+            await mediaCapture.InitializeAsync();
+
+            this.VideoSource.Source = mediaCapture;
+
+            // Get information about the preview
+            var previewProperties = mediaCapture.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.VideoPreview) as VideoEncodingProperties;
+
+            // Create the video frame to request a SoftwareBitmap preview frame
+            var videoFrame = new VideoFrame(BitmapPixelFormat.Bgra8, (int)previewProperties.Width, (int)previewProperties.Height);
+
+            await mediaCapture.StartPreviewAsync();
+
+            // Capture the preview frame
+            using (var currentFrame = await mediaCapture.GetPreviewFrameAsync(videoFrame))
+            {
+                // Collect the resulting frame
+                SoftwareBitmap previewFrame = currentFrame.SoftwareBitmap;
+
+                using (var stream = new InMemoryRandomAccessStream())
+                {
+                    var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
+                    encoder.SetSoftwareBitmap(previewFrame);
+
+                    await encoder.FlushAsync();
+
+                    var readStrem = stream.AsStreamForRead();
+                    var dataLean = readStrem.Length;
+                    var data = new byte[dataLean];
+                    await readStrem.ReadAsync(data, 0, data.Length);
+
+                    StorageFile newFile = await DownloadsFolder.CreateFileAsync("test.jpeg");
+                    using (var fileStream = await newFile.OpenStreamForWriteAsync())
+                    {
+                        await fileStream.WriteAsync(data, 0, data.Length);
+                    }
+                }
+
+                
+                // Add a simple green filter effect to the SoftwareBitmap
+                // EditPixels(previewFrame);
+            }
+
+            
+        }
+
+        //private unsafe void EditPixels(SoftwareBitmap bitmap)
+        //{
+        //    // Effect is hard-coded to operate on BGRA8 format only
+        //    if (bitmap.BitmapPixelFormat == BitmapPixelFormat.Bgra8)
+        //    {
+        //        // In BGRA8 format, each pixel is defined by 4 bytes
+        //        const int BYTES_PER_PIXEL = 4;
+
+        //        using (var buffer = bitmap.LockBuffer(BitmapBufferAccessMode.ReadWrite))
+        //        using (var reference = buffer.CreateReference())
+        //        {
+        //            // Get a pointer to the pixel buffer
+        //            byte* data;
+        //            uint capacity;
+        //            ((IMemoryBufferByteAccess)reference).GetBuffer(out data, out capacity);
+
+        //            // Get information about the BitmapBuffer
+        //            var desc = buffer.GetPlaneDescription(0);
+
+        //            // Iterate over all pixels
+        //            for (uint row = 0; row < desc.Height; row++)
+        //            {
+        //                for (uint col = 0; col < desc.Width; col++)
+        //                {
+        //                    // Index of the current pixel in the buffer (defined by the next 4 bytes, BGRA8)
+        //                    var currPixel = desc.StartIndex + desc.Stride * row + BYTES_PER_PIXEL * col;
+
+        //                    // Read the current pixel information into b,g,r channels (leave out alpha channel)
+        //                    var b = data[currPixel + 0]; // Blue
+        //                    var g = data[currPixel + 1]; // Green
+        //                    var r = data[currPixel + 2]; // Red
+
+        //                    // Boost the green channel, leave the other two untouched
+        //                    data[currPixel + 0] = b;
+        //                    data[currPixel + 1] = (byte)Math.Min(g + 80, 255);
+        //                    data[currPixel + 2] = r;
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ projects/VideoCameraStreamer/VideoCameraStreamer/Models/CameraModule.cs
@@ -0,0 +1,141 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CameraModule.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The camera module.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture;
+
+namespace VideoCameraStreamer.Models
+{
+    /// <summary>
+    /// The camera module.
+    /// </summary>
+    public class CameraModule : IDisposable
+    {
+        /// <summary>
+        /// Media Capture object for the USB camera
+        /// </summary>
+        private MediaCapture mediaCapture;
+
+        /// <summary>
+        /// The discovery.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public static async Task<IEnumerable<CameraModule>> Discovery()
+        {
+            var devices = await FindCameraDevice();
+
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the source.
+        /// </summary>
+        public MediaCapture Source
+        {
+            get { return this.mediaCapture; }
+        }
+
+        /// <summary>
+        /// The start.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public async Task Start()
+        {
+            try
+            {
+                await mediaCapture.StartPreviewAsync();
+            }
+            catch
+            {
+                Debug.WriteLine("UsbCamera: Failed to start camera preview stream");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Asynchronously initializes webcam feed
+        /// </summary>
+        /// <returns>
+        /// Task object: True if camera is successfully initialized; false otherwise.
+        /// </returns>
+        public async Task<bool> InitializeAsync()
+        {
+            if (mediaCapture != null)
+            {
+                return false;
+            }
+
+            // Attempt to get attached webcam
+            var cameraDevice = await FindCameraDevice();
+
+            if (cameraDevice == null)
+            {
+                // No camera found, report the error and break out of initialization
+                Debug.WriteLine("UsbCamera: No camera found!");
+
+// isInitialized = false;
+                return false;
+            }
+
+            // Creates MediaCapture initialization settings with foudnd webcam device
+           // var settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameraDevice.Id };
+            this.mediaCapture = new MediaCapture();
+
+            try
+            {
+                await mediaCapture.InitializeAsync();
+
+// isInitialized = true;
+                return true;
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Debug.WriteLine("UsbCamera: UnauthorizedAccessException: " + ex + "Ensure webcam capability is added in the manifest.");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("UsbCamera: Exception when initializing MediaCapture:" + ex);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// The dispose.
+        /// </summary>
+        public void Dispose()
+        {
+            this.mediaCapture?.Dispose();
+        }
+
+        /// <summary>
+        /// The find camera device.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        private static async Task<DeviceInformation> FindCameraDevice()
+        {
+            // Get available devices for capturing pictures
+            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+            return allVideoDevices.Count > 0 ? allVideoDevices[0] : null;
+        }
+    }
+}
