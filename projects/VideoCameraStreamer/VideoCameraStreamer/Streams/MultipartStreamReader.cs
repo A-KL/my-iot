5d1b5ab4b492750762196ad4be15f31eb6f36591
Anatolii Klots <myinfo256@yandex.ru>
2016-03-29 17:29:07 +0200

    Unit test

---
+++ projects/VideoCameraStreamer/VideoCameraStreamer/Streams/MultipartStreamReader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+
+namespace VideoCameraStreamer.Streams
+{
+    public class MultipartStream : Stream
+    {
+        public override bool CanRead { get; }
+
+        public override bool CanSeek { get; }
+
+        public override bool CanWrite { get; }
+
+        public override long Length { get; }
+
+        public override long Position { get; set; }
+
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MultipartStreamReader : IInputStream
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncOperationWithProgress<IBuffer, uint> IInputStream.ReadAsync(IBuffer buffer, uint count, InputStreamOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ projects/VideoCameraStreamer/VideoCameraStreamer/Streams/MultipleFileStream.cs
@@ -0,0 +1,109 @@
+﻿using System.IO;
+using System.Threading;
+using Windows.Storage;
+
+namespace VideoCameraStreamer.Streams
+{
+    public class DirectoryStream : Stream
+    {
+        private readonly CancellationToken loopToken;
+
+        private Stream currentFileStream;
+
+        private readonly StorageFolder folder;
+
+        private readonly string[] files;
+
+        private int index;
+
+
+        public DirectoryStream(StorageFolder folder)
+            : this(folder, CancellationToken.None)
+        { }
+
+        public DirectoryStream(StorageFolder folder, CancellationToken loopCancellationToken)
+        {
+            this.index = 0;
+            this.folder = folder;
+            //this.files = filePaths;
+
+            this.loopToken = loopCancellationToken;
+
+            this.OpenNextFile();
+        }
+
+        public bool IsLooped => this.loopToken != CancellationToken.None && !this.loopToken.IsCancellationRequested;
+
+        public override bool CanSeek => this.currentFileStream.CanSeek;
+
+        public override bool CanRead 
+            => this.IsLooped
+            || ((this.index == this.files.Length) && this.currentFileStream.CanRead);
+        
+        public override bool CanWrite => false;
+
+        public override long Length => this.currentFileStream?.Length ?? 0;
+
+        public override long Position
+        {
+            get
+            {
+                return this.currentFileStream?.Position ?? -1;
+            }
+            set
+            {
+                this.currentFileStream.Position = value;
+            }
+        }
+
+
+        public override void Flush()
+        {
+            this.currentFileStream.Flush();            
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var read = this.currentFileStream.Read(buffer, offset, count);
+
+            if (read < count)
+            {
+                this.OpenNextFile();
+            }
+
+            return read;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return this.currentFileStream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            this.currentFileStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OpenNextFile()
+        {
+            this.currentFileStream?.Dispose();
+
+            if (this.IsLooped)
+            {
+                this.index = 0;
+            }
+            else if (this.index == this.files.Length)
+            {
+                this.currentFileStream = null;
+                return;
+            }
+
+            this.currentFileStream = new FileStream(this.files[this.index++], FileMode.Open, FileAccess.Read);
+        }
+    }
+}
