f9bdfade032c82ea2f14653a06a63a85ec67e353
A-KL <myinfo256@yandex.ru>
2016-04-02 00:39:34 +0200

    WebSocket added

--- projects/WebServerDemo/src/MainPage.xaml.cs
+++ projects/WebServerDemo/src/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Reflection;
 
 using Microsoft.Practices.Unity;
@@ -7,8 +8,12 @@ using Microsoft.Iot.Web.Api;
 using Microsoft.Iot.Web.FileSystem;
 
 using System.Linq;
+using System.Text;
 using System.Web.Http;
 using Windows.Networking.Connectivity;
+using Windows.UI.Core;
+using Griffin.Net.Channels;
+using Griffin.Net.Protocols.Http.WebSocket;
 using Griffin.Networking.Web;
 using WebServerDemo.Model;
 
@@ -43,6 +48,29 @@ namespace WebServerDemo
             var server = new WebService(settings);
 
             server.Start(IPAddress.Parse(GetLocalIp()), 8000);
+
+            var socket = new WebSocketListener();
+            socket.Start(IPAddress.Parse(GetLocalIp()), 8001);
+           // socket.WebSocketClientConnect += Socket_WebSocketClientConnect;
+          //  socket.WebSocketClientConnected += Socket_WebSocketClientConnected;
+            socket.WebSocketMessageReceived = this.MessageReceived;
+        }
+
+
+        private void MessageReceived(ITcpChannel channel, object message)
+        {
+            var msg = message as IWebSocketMessage;
+
+            var data = new byte[msg.Payload.Length];
+
+            msg.Payload.Read(data, 0,(int) msg.Payload.Length);
+
+            var text = Encoding.ASCII.GetString(data);
+
+            this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                this.Label.Text = text;
+            });
         }
 
         private static string GetLocalIp()
