0778db27b3ea85fd19a7846ed6d3e5ccc87aba6a
A-KL <myinfo256@yandex.ru>
2016-04-16 21:31:49 +0200

    node js

--- projects/VideoCameraStreamer/NodejsWebServer/server.js
+++ projects/VideoCameraStreamer/NodejsWebServer/server.js
@@ -1,22 +1,46 @@
 var http = require('http');
-var addon = require("./MemoryStatusAddon");
+var uwp = require('uwp');
+//var addon = require("./MemoryStatusAddon");
 
-http.createServer(function (req, res) 
+uwp.projectNamespace("Windows.Devices");
+
+var gpioController = Windows.Devices.Gpio.GpioController.getDefault();
+
+var pin = gpioController.openPin(6);
+pin.setDriveMode(Windows.Devices.Gpio.GpioPinDriveMode.output)
+
+var currentValue = Windows.Devices.Gpio.GpioPinValue.high;
+pin.write(currentValue);
+
+http.createServer(function (req, res)
 {
-	//res.writeHead(200, { 'Content-Type': 'text/plain' });
-	//res.end('Hello World\n');
+    if (currentValue == Windows.Devices.Gpio.GpioPinValue.high)
+    {
+        currentValue = Windows.Devices.Gpio.GpioPinValue.low;
+    } 
+    else
+    {
+        currentValue = Windows.Devices.Gpio.GpioPinValue.high;
+    }
 
-	var memObj = addon.GlobalMemoryStatusEx();
+    pin.write(currentValue);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('LED value: ' + currentValue + '\n');
+
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.write('*************************************************\n');
-	res.write('Percent of memory in use: ' + memObj.load + '\n');
-	res.write('KB of physical memory: ' + memObj.physKb + '\n');
-	res.write('KB of free physical memory: ' + memObj.freePhysKb + '\n');
-	res.write('KB of paging file: ' + memObj.pageKb + '\n');
-	res.write('KB of free paging file: ' + memObj.freePageKb + '\n');
-	res.write('KB of virtual memory: ' + memObj.virtualKb + '\n');
-	res.write('KB of free virtual memory: ' + memObj.freeVirtualKb + '\n');
-	res.write('KB of free extended memory: ' + memObj.freeExtKb + '\n');
-	res.end('*************************************************\n');
+	res.end('Hello World\n');
+
+	//var memObj = addon.GlobalMemoryStatusEx();
+	//res.writeHead(200, { 'Content-Type': 'text/plain' });
+	//res.write('*************************************************\n');
+	//res.write('Percent of memory in use: ' + memObj.load + '\n');
+	//res.write('KB of physical memory: ' + memObj.physKb + '\n');
+	//res.write('KB of free physical memory: ' + memObj.freePhysKb + '\n');
+	//res.write('KB of paging file: ' + memObj.pageKb + '\n');
+	//res.write('KB of free paging file: ' + memObj.freePageKb + '\n');
+	//res.write('KB of virtual memory: ' + memObj.virtualKb + '\n');
+	//res.write('KB of free virtual memory: ' + memObj.freeVirtualKb + '\n');
+	//res.write('KB of free extended memory: ' + memObj.freeExtKb + '\n');
+	//res.end('*************************************************\n');
 
 }).listen(1337);
