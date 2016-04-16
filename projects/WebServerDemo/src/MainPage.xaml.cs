8770b93e669352eab03ff6e4a05796791a789160
A-KL <myinfo256@yandex.ru>
2016-04-17 00:25:50 +0200

    Video stream example

--- projects/WebServerDemo/src/MainPage.xaml.cs
+++ projects/WebServerDemo/src/MainPage.xaml.cs
@@ -12,7 +12,11 @@ using System.Text;
 using System.Web.Http;
 using Windows.Networking.Connectivity;
 using Windows.UI.Core;
+using Griffin.Core.Net.Protocols.Http.MJpeg;
+using Griffin.Net;
 using Griffin.Net.Channels;
+using Griffin.Net.Protocols.Http;
+using Griffin.Net.Protocols.Http.MJpeg;
 using Griffin.Net.Protocols.Http.WebSocket;
 using Griffin.Networking.Web;
 using WebServerDemo.Model;
@@ -23,9 +27,19 @@ namespace WebServerDemo
     {
         private const string DefaultPage = "index.html";
 
+        public IFramesSource source;
+
         public MainPage()
         {
             InitializeComponent();
+            
+            var imagesFolder =
+                Windows.ApplicationModel.Package.Current.InstalledLocation
+                .GetFolderAsync("Assets")
+                .GetAwaiter()
+                .GetResult();
+
+            source = new FilesFrameSource(imagesFolder);
 
             var container = new UnityContainer();
 
@@ -45,15 +59,33 @@ namespace WebServerDemo
             settings.Listeners.Add(new FileSystemListener("/", "wwwroot"));
             settings.Listeners.Add(new WebApiListener(assembly)); // use attribute routing            
 
+            // Http
             var server = new WebService(settings);
-
             server.Start(IPAddress.Parse(GetLocalIp()), 8000);
 
+            //WebSockets
             var socket = new WebSocketListener();
             socket.Start(IPAddress.Parse(GetLocalIp()), 8001);
             socket.WebSocketMessageReceived = this.MessageReceived;
+
+            //MJpeg
+            var config = new ChannelTcpListenerConfiguration(
+                    () => new HttpMessageDecoder(),
+                    () => new MJpegEncoder());
+
+            var liveVideoListeren = new HttpListener(config);
+            liveVideoListeren.MessageReceived = this.LiveStreamMessageReceived;
+            liveVideoListeren.Start(IPAddress.Parse(GetLocalIp()), 8002);           
         }
 
+        private void LiveStreamMessageReceived(ITcpChannel channel, object message)
+        {
+            var response = new HttpStreamResponse(HttpStatusCode.OK, "ok", "HTTP/1.1");
+
+            response.StreamSource = this.source;
+
+            channel.Send(response);
+        }
 
         private void MessageReceived(ITcpChannel channel, object message)
         {
@@ -61,7 +93,7 @@ namespace WebServerDemo
 
             var data = new byte[msg.Payload.Length];
 
-            msg.Payload.Read(data, 0,(int) msg.Payload.Length);
+            msg.Payload.Read(data, 0, (int)msg.Payload.Length);
 
             var text = Encoding.ASCII.GetString(data);
 
---
+++ projects/WebServerDemo/src/Model/FilesFrameSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Storage;
+using Griffin.Core.Net.Protocols.Http.MJpeg;
+
+namespace WebServerDemo.Model
+{
+    public class FilesFrameSource : IFramesSource
+    {
+        private readonly IStorageFolder rootFolder;
+
+        public FilesFrameSource(IStorageFolder folder)
+        {
+            this.rootFolder = folder;
+        }
+        
+        public IEnumerable<IImageFrame> Frames
+        {
+            get
+            {
+                foreach (var storageFile in this.rootFolder.GetFilesAsync().GetAwaiter().GetResult())
+                {
+                    yield return new ImageFileFrame(storageFile);
+                }              
+            }
+        }
+    }
+}
