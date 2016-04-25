3ac2b72482dd05ddd9f6a060b2198b0960d92eff
Anatolii Klots <Anatolii.Klots@albumprinter.com>
2016-04-25 16:20:03 +0200

    Cleanup and optimization

--- projects/VideoCameraStreamer/VideoCameraStreamer/MainPage.xaml.cs
+++ projects/VideoCameraStreamer/VideoCameraStreamer/MainPage.xaml.cs
@@ -10,7 +10,6 @@ using Griffin.Net.Protocols.Http.WebSocket;
 using Griffin.Networking.Web;
 using Microsoft.Iot.Web;
 using Microsoft.Iot.Web.FileSystem;
-using Microsoft.Iot.Web.Streaming;
 using Microsoft.Practices.Unity;
 using System;
 using Windows.UI.Xaml.Controls;
@@ -58,51 +57,11 @@ namespace VideoCameraStreamer
             this.InitNetwork();
         }
 
-        //private unsafe void EditPixels(SoftwareBitmap bitmap)
-        //{
-        //    // Effect is hard-coded to operate on BGRA8 format only
-        //    if (bitmap.BitmapPixelFormat == BitmapPixelFormat.Bgra8)
-        //    {
-        //        // In BGRA8 format, each pixel is defined by 4 bytes
-        //        const int BYTES_PER_PIXEL = 4;
-
-        //        using (var buffer = bitmap.LockBuffer(BitmapBufferAccessMode.ReadWrite))
-        //        using (var reference = buffer.CreateReference())
-        //        {
-        //            // Get a pointer to the pixel buffer
-        //            byte* data;
-        //            uint capacity;
-        //            ((IMemoryBufferByteAccess)reference).GetBuffer(out data, out capacity);
-
-        //            // Get information about the BitmapBuffer
-        //            var desc = buffer.GetPlaneDescription(0);
-
-        //            // Iterate over all pixels
-        //            for (uint row = 0; row < desc.Height; row++)
-        //            {
-        //                for (uint col = 0; col < desc.Width; col++)
-        //                {
-        //                    // Index of the current pixel in the buffer (defined by the next 4 bytes, BGRA8)
-        //                    var currPixel = desc.StartIndex + desc.Stride * row + BYTES_PER_PIXEL * col;
-
-        //                    // Read the current pixel information into b,g,r channels (leave out alpha channel)
-        //                    var b = data[currPixel + 0]; // Blue
-        //                    var g = data[currPixel + 1]; // Green
-        //                    var r = data[currPixel + 2]; // Red
-
-        //                    // Boost the green channel, leave the other two untouched
-        //                    data[currPixel + 0] = b;
-        //                    data[currPixel + 1] = (byte)Math.Min(g + 80, 255);
-        //                    data[currPixel + 2] = r;
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
-
         private void InitNetwork()
         {
             var container = new UnityContainer();
+
+            var ip = IPAddress.Parse(NetworkHelper.GetLocalIp());
 
             // container.RegisterType<IWeatherService, FakeWeatherService>(new HierarchicalLifetimeManager());
 
@@ -120,11 +79,11 @@ namespace VideoCameraStreamer
 
             // Http
             var server = new WebService(settings);
-            server.Start(IPAddress.Parse(GetLocalIp()), 8000);
+            server.Start(ip, 8000);
 
             //WebSockets
             var socket = new WebSocketListener();
-            socket.Start(IPAddress.Parse(GetLocalIp()), 8001);
+            socket.Start(ip, 8001);
             socket.WebSocketMessageReceived = this.MessageReceived;
 
             //MJpeg
@@ -134,7 +93,7 @@ namespace VideoCameraStreamer
 
             var liveVideoListeren = new HttpListener(config);
             liveVideoListeren.MessageReceived = this.LiveStreamMessageReceived;
-            liveVideoListeren.Start(IPAddress.Parse(GetLocalIp()), 8002);
+            liveVideoListeren.Start(ip, 8002);
         }
 
         private void LiveStreamMessageReceived(ITcpChannel channel, object message)
@@ -160,22 +119,6 @@ namespace VideoCameraStreamer
             //{
             //    this.Label.Text = text;
             //});
-        }
-
-        private static string GetLocalIp()
-        {
-            var icp = NetworkInformation.GetInternetConnectionProfile();
-
-            if (icp?.NetworkAdapter == null) return null;
-            var hostname =
-                NetworkInformation.GetHostNames()
-                    .SingleOrDefault(
-                        hn =>
-                            hn.IPInformation?.NetworkAdapter != null && hn.IPInformation.NetworkAdapter.NetworkAdapterId
-                            == icp.NetworkAdapter.NetworkAdapterId);
-
-            // the ip address
-            return hostname?.CanonicalName;
         }
     }
 }
---
+++ projects/VideoCameraStreamer/VideoCameraStreamer/Models/NetworkHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace VideoCameraStreamer.Models
+{
+    using System.Linq;
+    using Windows.Networking.Connectivity;
+
+    public static  class NetworkHelper
+    {
+        public static string GetLocalIp()
+        {
+            var icp = NetworkInformation.GetInternetConnectionProfile();
+
+            if (icp?.NetworkAdapter == null) return null;
+            var hostname =
+                NetworkInformation.GetHostNames()
+                    .SingleOrDefault(
+                        hn =>
+                            hn.IPInformation?.NetworkAdapter != null && hn.IPInformation.NetworkAdapter.NetworkAdapterId
+                            == icp.NetworkAdapter.NetworkAdapterId);
+
+            return hostname?.CanonicalName;
+        }
+    }
+}
