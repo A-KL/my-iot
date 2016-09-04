d6dc24927b02c2aabf0f9387e270862bf8f973f7
A-KL <myinfo256@yandex.ru>
2016-09-04 21:49:45 +0200

    Works!

---
+++ projects/Voicy/AudioEffects/PitchAudioEffect.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Effects;
+using Windows.Media.MediaProperties;
+
+namespace AudioEffects
+{
+    // Using the COM interface IMemoryBufferByteAccess allows us to access the underlying byte array in an AudioFrame
+    [ComImport]
+    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    unsafe interface IMemoryBufferByteAccess
+    {
+        void GetBuffer(out byte* buffer, out uint capacity);
+    }
+
+    public sealed class PitchAudioEffect : IBasicAudioEffect
+    {
+        private List<AudioEncodingProperties> supportedEncodingProperties;
+        private AudioEncodingProperties currentEncodingProperties;
+        private IPropertySet propertySet;
+
+        public PitchAudioEffect()
+        {
+            this.supportedEncodingProperties = new List<AudioEncodingProperties>();
+
+            // Support 44.1kHz and 48kHz mono float
+            AudioEncodingProperties encodingProps1 = AudioEncodingProperties.CreatePcm(44100, 1, 32);
+            encodingProps1.Subtype = MediaEncodingSubtypes.Float;
+
+            AudioEncodingProperties encodingProps2 = AudioEncodingProperties.CreatePcm(48000, 1, 32);
+            encodingProps2.Subtype = MediaEncodingSubtypes.Float;
+
+            AudioEncodingProperties encodingProps3 = AudioEncodingProperties.CreatePcm(16000, 1, 16);
+            encodingProps3.Subtype = MediaEncodingSubtypes.Float;
+            supportedEncodingProperties.Add(encodingProps1);
+            supportedEncodingProperties.Add(encodingProps2);
+            supportedEncodingProperties.Add(encodingProps3);
+         }
+
+        private float Value
+        {
+            get { return (float)propertySet["Value"]; }
+        }
+
+        public IReadOnlyList<AudioEncodingProperties> SupportedEncodingProperties
+        {
+            get
+            {
+                return this.supportedEncodingProperties;
+            }
+        }
+
+        public bool UseInputFrameForOutput
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+
+        public void Close(MediaEffectClosedReason reason)
+        {
+
+        }
+
+        public void DiscardQueuedFrames()
+        {
+
+        }
+
+        public void SetEncodingProperties(AudioEncodingProperties encodingProperties)
+        {
+            currentEncodingProperties = encodingProperties;
+        }
+
+        public void SetProperties(IPropertySet configuration)
+        {
+            this.propertySet = configuration;
+        }
+
+        unsafe public void ProcessFrame(ProcessAudioFrameContext context)
+        {
+            AudioFrame inputFrame = context.InputFrame;
+            AudioFrame outputFrame = context.OutputFrame;
+
+            using (AudioBuffer inputBuffer = inputFrame.LockBuffer(AudioBufferAccessMode.Read),
+                                outputBuffer = outputFrame.LockBuffer(AudioBufferAccessMode.Write))
+            using (IMemoryBufferReference inputReference = inputBuffer.CreateReference(),
+                                            outputReference = outputBuffer.CreateReference())
+            {
+                byte* inputDataInBytes;
+                byte* outputDataInBytes;
+                uint inputCapacity;
+                uint outputCapacity;
+
+                ((IMemoryBufferByteAccess)inputReference).GetBuffer(out inputDataInBytes, out inputCapacity);
+                ((IMemoryBufferByteAccess)outputReference).GetBuffer(out outputDataInBytes, out outputCapacity);
+
+                float* inputDataInFloat = (float*)inputDataInBytes;
+                float* outputDataInFloat = (float*)outputDataInBytes;
+
+                int dataInFloatLength = (int)inputBuffer.Length / sizeof(float);
+
+                PitchShifter.PitchShift(this.Value, dataInFloatLength, (long)1024, (long)10, this.currentEncodingProperties.SampleRate, inputDataInFloat, outputDataInFloat);
+            }
+
+        }        
+    }
+}
--- projects/Voicy/Voicy/MainPage.xaml.cs
+++ projects/Voicy/Voicy/MainPage.xaml.cs
@@ -1,23 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using AudioEffects;
+using System;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
-using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Media.Audio;
+using Windows.Media.Effects;
 using Windows.Media.MediaProperties;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Voicy
 {
@@ -26,9 +18,158 @@ namespace Voicy
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private EqualizerEffectDefinition equalizer;
+        private ReverbEffectDefinition reverb;
+
+        private AudioGraph graph;
+        private AudioDeviceInputNode deviceInputNode;
+        private AudioDeviceOutputNode deviceOutputNode;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Devices
+
+            var outputDevice = await GetDefaultDeviceAsync(DeviceClass.AudioRender);
+            if (outputDevice == null)
+            {
+                return;
+            }
+
+            // Graph
+
+            var settings = new AudioGraphSettings(Windows.Media.Render.AudioRenderCategory.Media);
+            
+            settings.PrimaryRenderDevice = outputDevice;
+
+            settings.EncodingProperties = AudioEncodingProperties.CreatePcm(24000, 1, 16);
+            
+            var result = await AudioGraph.CreateAsync(settings);
+
+            if (result.Status != AudioGraphCreationStatus.Success)
+            {
+                return;
+            }
+
+            this.graph = result.Graph;
+
+            // Output
+
+            var outputDeviceResult = await graph.CreateDeviceOutputNodeAsync();
+
+            if (outputDeviceResult.Status != AudioDeviceNodeCreationStatus.Success)
+            {
+                return;
+            }
+
+            this.deviceOutputNode = outputDeviceResult.DeviceOutputNode;
+
+            // Input
+            var inputDeviceResult = await graph.CreateDeviceInputNodeAsync(Windows.Media.Capture.MediaCategory.Other);
+            if (inputDeviceResult.Status != AudioDeviceNodeCreationStatus.Success)
+            {
+                return;
+            }
+
+            this.deviceInputNode = inputDeviceResult.DeviceInputNode;
+            
+            this.deviceInputNode.AddOutgoingConnection(this.deviceOutputNode);
+
+            // Effects
+
+            // Reverb
+            //this.CreateReverbEffect(graph);
+
+            // outputNode.EffectDefinitions.Add(this.reverb);
+
+            // Eualizer
+            //this.CreateEqEffect(graph);
+
+            //outputNode.EffectDefinitions.Add(equalizer);
+
+            // Custom
+            // Create a property set and add a property/value pair
+            PropertySet properties = new PropertySet();
+
+            properties.Add("Value", 0.8f);
+
+            var pitch = new AudioEffectDefinition(typeof(PitchAudioEffect).FullName, properties);
+
+            this.deviceOutputNode.EffectDefinitions.Add(pitch);
+
+            // Done
+            graph.Start();
+        }
+
+        private void slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            return;
+
+            if (this.equalizer == null)
+            {
+                return;
+            }
+
+            var slider = sender as Slider;
+
+            var value = ConvertRange(slider.Value);
+
+            if (slider == this.slider0)
+            {
+                this.equalizer.Bands[0].Gain = value;
+            }
+            else if (slider == this.slider1)
+            {
+                this.equalizer.Bands[1].Gain = value;
+            }
+            else if (slider == this.slider2)
+            {
+                this.equalizer.Bands[2].Gain = value;
+            }
+            else if (slider == this.slider3)
+            {
+                this.equalizer.Bands[3].Gain = value;
+            }
+        }
+
+        private void CreateEqEffect(AudioGraph graph)
+        {
+            // See the MSDN page for parameter explanations
+            // https://msdn.microsoft.com/en-us/library/windows/desktop/microsoft.directx_sdk.xapofx.fxeq_parameters(v=vs.85).aspx
+            this.equalizer = new EqualizerEffectDefinition(graph);
+            this.equalizer.Bands[0].FrequencyCenter = 20.0f;
+            this.equalizer.Bands[0].Gain = 4.033f;
+            this.equalizer.Bands[0].Bandwidth = 1.5f;
+
+            this.equalizer.Bands[1].FrequencyCenter = 200.0f;
+            this.equalizer.Bands[1].Gain = 1.6888f;
+            this.equalizer.Bands[1].Bandwidth = 1.5f;
+
+            this.equalizer.Bands[2].FrequencyCenter = 10000.0f;
+            this.equalizer.Bands[2].Gain = 0.128;
+            this.equalizer.Bands[2].Bandwidth = 1.5f;
+
+            this.equalizer.Bands[3].FrequencyCenter = 20000.0f;
+            this.equalizer.Bands[3].Gain = 0.128;
+            this.equalizer.Bands[3].Bandwidth = 2.0f;
+        }
+
+        private void CreateReverbEffect(AudioGraph graph)
+        {
+            // Create reverb effect
+            this.reverb = new ReverbEffectDefinition(graph);
+
+            // See the MSDN page for parameter explanations
+            // https://msdn.microsoft.com/en-us/library/windows/desktop/microsoft.directx_sdk.xaudio2.xaudio2fx_reverb_parameters(v=vs.85).aspx
+            this.reverb.WetDryMix = 50;
+            this.reverb.ReflectionsDelay = 120;
+            this.reverb.ReverbDelay = 30;
+            this.reverb.RearDelay = 3;
+            this.reverb.DecayTime = 0.5;
         }
 
         private async Task<DeviceInformation> GetDefaultDeviceAsync(DeviceClass deviceClass)
@@ -40,84 +181,15 @@ namespace Voicy
             return audioDevice;
         }
 
-        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        // Mapping the 0-100 scale of the slider to a value between the min and max gain
+        private double ConvertRange(double value)
         {
-            // Devices
+            // These are the same values as the ones in xapofx.h
+            const double fxeq_min_gain = 0.126;
+            const double fxeq_max_gain = 7.94;
 
-            var outputDevice = await GetDefaultDeviceAsync(DeviceClass.AudioRender);
-
-           // var inputDevice = await GetDefaultDeviceAsync(DeviceClass.AudioCapture);
-
-            if ( outputDevice == null)
-            {
-                return;
-            }
-
-            // Graph
-
-            var settings = new AudioGraphSettings(Windows.Media.Render.AudioRenderCategory.Speech);
-
-            settings.PrimaryRenderDevice = outputDevice;
-
-            var result = await AudioGraph.CreateAsync(settings);
-
-            if (result.Status != AudioGraphCreationStatus.Success)
-            {
-                return;
-            }
-
-            var graph = result.Graph;
-
-            // Output
-
-            var outputDeviceResult = await graph.CreateDeviceOutputNodeAsync();
-
-            if (outputDeviceResult.Status != AudioDeviceNodeCreationStatus.Success)
-            {
-                return;
-            }
-
-            var outputNode = outputDeviceResult.DeviceOutputNode;
-
-            // Input
-
-            var inputDeviceResult = await graph.CreateDeviceInputNodeAsync(Windows.Media.Capture.MediaCategory.Speech);
-
-            if (inputDeviceResult.Status != AudioDeviceNodeCreationStatus.Success)
-            {
-                return;
-            }
-
-            var inputNode = inputDeviceResult.DeviceInputNode;
-
-            EchoEffectDefinition echoEffectDefinition = new EchoEffectDefinition(graph);
-            echoEffectDefinition.Delay = 100.0f;
-            echoEffectDefinition.WetDryMix = 0.7f;
-            echoEffectDefinition.Feedback = 0.5f;
-            //inputNode.EffectDefinitions.Add(echoEffectDefinition);
-
-            ReverbEffectDefinition reverbEffectDefinition = new ReverbEffectDefinition(graph);
-            reverbEffectDefinition.DecayTime = 0.05;
-            //reverbEffectDefinition.// = 0.005;
-            inputNode.EffectDefinitions.Add(reverbEffectDefinition);
-            
-            EqualizerEffectDefinition equalizer = new EqualizerEffectDefinition(graph);
-            var b1 = equalizer.Bands[0];
-            var b2 = equalizer.Bands[1];
-            var b3 = equalizer.Bands[2];
-            var b4 = equalizer.Bands[3];
-            
-            b1.Bandwidth = 1;
-            b1.Gain = 6;
-            b1.FrequencyCenter = 30;
-
-            inputNode.EffectDefinitions.Add(equalizer);
-
-            inputNode.AddOutgoingConnection(outputNode);
-
-            // Done!
-
-            graph.Start();
+            double scale = (fxeq_max_gain - fxeq_min_gain) / 100;
+            return (fxeq_min_gain + ((value) * scale));
         }
     }
 }
